bc4be0337f647f21171594633c29b35cfeaf6b2a
acetelles <115594718+acetelles@users.noreply.github.com>
2023-08-21 18:42:44 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,39 +1,61 @@
-var song1 = "";
-var song2 = "";
-var leftWrist = 0;
-var rightWrist = 0;
+objectDetector = "";
+img = "";
+objects = [];
+status ="";
 
 
-function preload() {
-    song1 = loadsound("music.mp3");
-    song2 = loadsound("music2.mp3");
-} 
+function preload(){
+    img = loadImage('dog_cat.jpg');
+}
 
 function setup() {
-    canvas = createCanvas(500, 500);
+    canvas = createCanvas(380, 380);
     canvas.center();
-
     video = createCapture(VIDEO);
+    video.size(380, 380);
     video.hide();
-    
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Estatus: detectando objetos";
+}
+
+
+function modelLoaded() {
+    console.log("Modelo Cargado!!");
+    status = true;
+    objectDetector.detect(video, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
 
 function draw() {
-    image(video, 0, 0, 600, 500);
-    Fill('#FF0000');
-    stroke('#FF0000');
+    image(video, 0, 0, 380, 380);
+    if(status != "")
+    {
+        r = random(255);
+        g = random (255);
+        b = random(255);
+        objectDetector.detect(video, gotResult);
+        for (var i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Estatus: objeto detectado";
+            document.getElementById("number_of_objects").innerHTML = "Número de objetos detectados : " + objectos.length;
+
+            fill(r,g,b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
 
-function gotPoses() {
-    if(results.length > 0){
-        console.log(results);   
-        leftWristX = results[0].pose.leftWrist.x;
-        leftWristY = results[0].pose.leftWrist.y;
-        console.log("leftWristX = " + leftwrist + "leftWrist = " + leftWristY)
 
-        rightWristX = results[0].pose.rightWrist.x;
-        rightWristY = results[0].pose.rightWrist.y;
-        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
-}
+
+
+
